d0dedebbbb63e84945ded9d66dd8f8cdebfe2ab5
razielll <asto1387@gmail.com>
2018-07-25 14:10:33 +0300

    pckgfix

---
+++ backend/services/eventiService.js
@@ -0,0 +1,65 @@
+const ObjectId = require('mongodb').ObjectId;
+const mongoService = require('./mongoService.js')
+
+function query() {
+
+
+    return mongoService.connect()
+        .then(db => {
+            const collection = db.collection('eventi');
+            return collection.find({}).toArray()
+        })
+}
+
+function remove(eventiId) {
+    eventiId = new ObjectId(eventiId)
+    return mongoService.connect()
+        .then(db => {
+            const collection = db.collection('eventi');
+            return collection.remove({ _id: eventiId })
+        })
+}
+
+
+function getById(eventiId) {
+    eventiId = new ObjectId(eventiId)
+    return mongoService.connect()
+        .then(db => {
+            const collection = db.collection('eventi');
+            return collection.findOne({ _id: eventiId })
+        })
+}
+
+function add(eventi) {
+    return mongoService.connect()
+        .then(db => {
+            const collection = db.collection('eventi');
+            return collection.insertOne(eventi)
+                .then(result => {
+                    eventi._id = result.insertedId;
+                    return eventi;
+                })
+        })
+}
+
+function update(eventi) {
+    eventi._id = new ObjectId(eventi._id)
+    console.log('new eventi', eventi)
+    return mongoService.connect()
+        .then(db => {
+            const collection = db.collection('eventi');
+            return collection.updateOne({ _id: eventi._id }, { $set: eventi })
+                .then(res => {
+                    console.log('eventi!!')
+                    return eventi;
+                })
+        })
+}
+
+module.exports = {
+    query,
+    remove,
+    getById,
+    add,
+    update
+}

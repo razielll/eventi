3b5e0e579a4fe8887a4bafcad6af44afbfe9641c
razielll <asto1387@gmail.com>
2018-07-28 14:17:19 +0300

    syntax after pull errrs

--- eventi/src/store/userModule.js
+++ eventi/src/store/userModule.js
@@ -4,31 +4,28 @@ export default {
     state: {
         user: {}
     },
-    mutations: {},
+    mutations: {
+        setUser(state, { user }) {
+            state.user = user
+        },
+    },
     actions: {
         userSignup(context, { user }) {
             userService.userSignup(user);
         },
-        mutations: {
-            setUser(state, { user }) {
-                state.user = user
-            }
+        userSignup(context, { user }) {
+            userService.userSignup(user)
+                .then(user => context.commit({ type: 'setUser', user }))
         },
-        actions: {
-            userSignup(context, { user }) {
-                userService.userSignup(user)
-                    .then(user => context.commit({ type: 'setUser', user }))
-            },
-            userLogin(context, { user }) {
-                userService.userLogin(user)
-                    .then(user => {
-                        context.commit({ type: 'setUser', user })
-                    })
-            }
+        userLogin(context, { user }) {
+            userService.userLogin(user)
+                .then(user => {
+                    context.commit({ type: 'setUser', user })
+                })
         },
         incEventiClap(context, eventiId) {
             // TODO update user eventi
             console.log('TODO: update user eventi clap');
         },
-    }
+    },
 };

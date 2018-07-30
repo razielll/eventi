8c0e9650f37c7941e6c5dc75dab66cb11379c560
Ariel Zusman <ariel.zusman@gmail.com>
2018-07-30 23:38:09 +0300

    added filter ui

--- eventi/src/components/navBar.vue
+++ eventi/src/components/navBar.vue
@@ -1,30 +1,67 @@
 <template>
+    <section class="">
+    <nav class="conatiner navbar is-transparent" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <router-link to="/" class="navbar-item logo title"><span class="logo-letter">e</span>venti </router-link>
+            <!-- <div class="navbar-burger burger" data-target="navBarMenu">
+            <span></span>
+            <span></span>
+            <span></span> -->
+            <!-- </div> -->
+        </div>
+        <div class="navbar-menu" id="navBarMenu">
+            <div class="navbar-end" v-if="!user">
+                <router-link to="/" class="navbar-item">eventi</router-link>
+                <a class="navbar-item" v-if="!user" @click="userLogin = !userLogin">login</a>
+                <a class="navbar-item" v-if="!user" @click="userSignup = !userSignup">signup</a>
+            </div>
+            <div class="navbar-end" v-if="user">
+                <router-link to="/" class="navbar-item">eventi</router-link>
+                <router-link to="/eventi/edit" class="navbar-item">New Eventi</router-link>
+                <router-link to="/user" class="navbar-item">{{user}} profile</router-link>
+                <a class="navbar-item" @click="logOut">logout</a>
+            </div>
+            <userLoginModal :class="{'is-active': userLogin}" @close-modal="closeModal" @go-signup="signup" />
+            <userSignupModal :class="{'is-active': userSignup}" @close-modal="closeModal" />
+        </div>
+
+    </nav>
+    <hr>
+    <div class="container">
+      <div class="columns filters">
+        <div class="column is-8-tablet">
+          <div class="buttons">
+            <button class="button">Lecture</button>
+            <button class="button">Party</button>
+            <button class="button">Gathering</button>
+            <button class="button">Sale</button>
+            <button class="button">Need Help</button>
+            <button class="button">Lost &amp; Found</button>
+          </div>
+        </div>
+        <div class="column">
+          <div class="field">
+            <input class="is-checkradio" id="distance-1" type="radio" name="exampleRadioInline" checked="checked">
+            <label for="distance-1">500m</label>
+            <input class="is-checkradio" id="distance-2" type="radio" name="exampleRadioInline">
+            <label for="distance-2">1000m</label>
+          </div>
+        </div>
+        <div class="column">
+          <div class="field">
+            <p class="control has-icons-left has-icons-right">
+              <input class="input" type="search" placeholder="Search for location">
+              <span class="icon is-small is-left">
+                <font-awesome-icon icon="search"/>
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
     
-<nav class="navbar is-transparent" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-     <router-link to="/" class="navbar-item logo title"><span class="logo-letter">e</span>venti </router-link>
-    <!-- <div class="navbar-burger burger" data-target="navBarMenu">
-      <span></span>
-      <span></span>
-      <span></span> -->
-    <!-- </div> -->
-  </div>
-  <div class="navbar-menu" id="navBarMenu">
-    <div class="navbar-end" v-if="!user">
-      <router-link to="/" class="navbar-item">eventi</router-link> 
-      <a class="navbar-item" v-if="!user" @click="userLogin = !userLogin">login</a>
-      <a class="navbar-item" v-if="!user" @click="userSignup = !userSignup">signup</a>
-    </div>
-    <div class="navbar-end" v-if="user">
-      <router-link to="/" class="navbar-item">eventi</router-link> 
-      <router-link to="/eventi/edit" class="navbar-item">New Eventi</router-link> 
-      <router-link to="/user" class="navbar-item" >{{user}} profile</router-link>
-      <a class="navbar-item"  @click="logOut">logout</a> 
-    </div>
-      <userLoginModal :class="{'is-active': userLogin}" @close-modal="closeModal" @go-signup="signup"/>
-      <userSignupModal :class="{'is-active': userSignup}" @close-modal="closeModal"/>
-  </div>
-</nav>
+    <hr>
+    </section>
 </template>
 
 <script>
@@ -97,15 +134,31 @@ nav {
     transition: 0.3s;
     font-weight: bold;
     color: #2c3e50;
-
+    position: relative;
+    margin: 0 5px;
+    text-transform: capitalize;
     &.router-link-exact-active {
       color: #42b983;
     }
+    &:after {
+      content: '';
+      height: 3px;
+      width: 0;
+      position: absolute;
+      background-color: #42b983;
+      bottom: 0;
+      left: 0;
+      transition: 0.3s;
+    }
     &:hover {
-      transform: scale(1.3);
-      color: #41b883;
-      padding: 1em;
+      color: #42b983;
+    }
+    &:hover::after {
+      width: 100%;
     }
   }
+}
+.filters {
+  align-items: center;
 }
 </style>
--- eventi/src/main.js
+++ eventi/src/main.js
@@ -5,6 +5,7 @@ import store from './store';
 import VeeValidate from 'vee-validate';
 import axios from 'axios';
 import 'bulma/css/bulma.css';
+import 'bulma-checkradio/dist/css/bulma-checkradio.min.css';
 import 'animate.css';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -13,12 +14,22 @@ import {
   faCheck,
   faLock,
   faUser,
-  faExclamationTriangle
+  faExclamationTriangle,
+  faLocationArrow,
+  faSearch
 } from '@fortawesome/free-solid-svg-icons';
 
 axios.defaults.withCredentials = true;
 
-library.add(faLock, faCheck, faEnvelope, faUser, faExclamationTriangle);
+library.add(
+  faLock,
+  faCheck,
+  faEnvelope,
+  faUser,
+  faExclamationTriangle,
+  faLocationArrow,
+  faSearch
+);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 import 'bulma/css/bulma.css';

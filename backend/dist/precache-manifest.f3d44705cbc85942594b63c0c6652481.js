a0e5b2fd4861960179e0ac8a165e846661fc0da7
Ariel Zusman <ariel.zusman@gmail.com>
2018-08-01 11:52:43 +0300

    added dist to backend

---
+++ backend/dist/precache-manifest.f3d44705cbc85942594b63c0c6652481.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "38041405b0869c46aa00",
+    "url": "/js/chunk-vendors.38041405.js"
+  },
+  {
+    "revision": "41c8864ebce817c7b539",
+    "url": "/js/app.41c8864e.js"
+  },
+  {
+    "revision": "cdc9da7a9e1fc1b52ea0106a7b184f83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddaf7c2155d7d4de74c336ac0409e6e9",
+    "url": "/img/clap.ddaf7c21.png"
+  },
+  {
+    "revision": "38041405b0869c46aa00",
+    "url": "/css/chunk-vendors.32ce83eb.css"
+  },
+  {
+    "revision": "41c8864ebce817c7b539",
+    "url": "/css/app.39cb7079.css"
+  }
+];

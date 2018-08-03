0f0b8897bbe98dc4e223ec62ca0ccd6775c39dcd
Ariel Zusman <ariel.zusman@gmail.com>
2018-08-03 18:34:57 +0300

    socket io change

--- backend/server.js
+++ backend/server.js
@@ -11,42 +11,42 @@ const app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-io.on('connection', function (socket) {
-	console.log('a user connected');
-	socket.on('chat join', function () {
-		socket.broadcast.emit('broadcast-join-chat', 'hello friends!');
-	});
-	socket.on('disconnect', function () {
-		console.log('user disconnected');
-	});
+io.on('connection', function(socket) {
+  console.log('a user connected');
+  socket.on('chat join', function() {
+    socket.broadcast.emit('broadcast-join-chat', 'hello friends!');
+  });
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
 
-	socket.on('SEND_MESSAGE', function (msg) {
-		console.log('server got msg from', msg.user, ' sent: ', msg.txt);
-		msg.user === undefined ? (msg.user = '') : msg.user;
-		io.emit('messageChannel', msg);
-		// io.emit('messageChannel', `${msg.user}: ${msg.txt}`);
-		// socket.broadcast.emit(`broadcastMsg`,`${msg.user}: ${msg.txt}`);
-	});
+  socket.on('SEND_MESSAGE', function(msg) {
+    console.log('server got msg from', msg.user, ' sent: ', msg.txt);
+    msg.user === undefined ? (msg.user = '') : msg.user;
+    io.emit('messageChannel', msg);
+    // io.emit('messageChannel', `${msg.user}: ${msg.txt}`);
+    socket.broadcast.emit(`broadcastMsg`, `${msg.user}: ${msg.txt}`);
+  });
 });
 server.listen(33333);
 
 app.use(cookieParser());
 app.use(
-	cors({
-		origin: ['http://localhost:8080'],
-		credentials: true // enable set cookie
-	})
+  cors({
+    origin: ['http://localhost:8080'],
+    credentials: true // enable set cookie
+  })
 );
 app.use(
-	session({
-		secret: 'puki muki',
-		resave: false,
-		saveUninitialized: true,
-		cookie: {
-			secure: false,
-			maxAge: 600000
-		}
-	})
+  session({
+    secret: 'puki muki',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: false,
+      maxAge: 600000
+    }
+  })
 );
 
 const addUserRoute = require('./routes/userRoute');
@@ -62,7 +62,7 @@ addEventiRoute(app);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-	console.log(`App server litening on port ${port}!`);
+  console.log(`App server litening on port ${port}!`);
 });
 
 // var options = {

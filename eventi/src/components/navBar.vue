f56c022914e5989ee02cf2b0f98a238f041ef16b
Ariel Zusman <ariel.zusman@gmail.com>
2018-08-15 09:07:42 +0300

    changed menu items

--- eventi/src/components/navBar.vue
+++ eventi/src/components/navBar.vue
@@ -20,13 +20,13 @@
         </div>
         <div class="navbar-menu" id="navBarMenu" :class="{'is-active': toggledBurger}">
             <div class="navbar-end" v-if="!user">
-                <router-link to="/" class="navbar-item" @click.native="toggledBurger = !toggledBurger">eventi</router-link>
+                <router-link to="/" class="navbar-item" @click.native="toggledBurger = !toggledBurger">Home</router-link>
                 <a class="navbar-item" @click="userLogin = !userLogin;"> login</a>
                 <a class="navbar-item" @click="userSignup = !userSignup">signup</a>
             </div>
             <div class="navbar-end" v-if="user" >
-                <router-link to="/" class="navbar-item" @click.native="toggledBurger = !toggledBurger">eventi</router-link>
-                <router-link to="/eventi/edit" class="navbar-item" @click.native="toggledBurger = !toggledBurger">New Eventi</router-link>
+                <router-link to="/" class="navbar-item" @click.native="toggledBurger = !toggledBurger">Home</router-link>
+                <router-link to="/eventi/edit" class="navbar-item" @click.native="toggledBurger = !toggledBurger">Create Eventi</router-link>
                 <router-link to="/user" class="navbar-item" @click.native="toggledBurger = !toggledBurger"> {{user}} profile</router-link>
                 <a class="navbar-item" @click="logOut">logout</a>
             </div>
